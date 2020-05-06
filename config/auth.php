27cec247df01c01bd232b15854596f350205ec90
nyacore <bjadmund@mail.ru>
2020-05-06 19:31:03 +0400

    App\User => App\Models\User

--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-    
+
         'api' => [
             'driver' => 'jwt',
             'provider' => 'users',
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
